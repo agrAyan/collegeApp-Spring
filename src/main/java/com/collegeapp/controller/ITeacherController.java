7882666ca6b394766d3dd2f207f926aaf9e2960b
agrAyan <87567431+agrAyan@users.noreply.github.com>
2021-10-18 13:10:28 +0530

    Update ITeacherController.java

--- src/main/java/com/collegeapp/controller/ITeacherController.java
+++ src/main/java/com/collegeapp/controller/ITeacherController.java
@@ -28,11 +28,13 @@ public class ITeacherController {
 	@Autowired
 	ITeacherService teacherService;
 	
+	//url to add a teacher
 	@PostMapping("/teacher")
 	public Teacher addTeacher(@RequestBody Teacher teacher) {
 		return teacherService.addTeacher(teacher);
 	}
 	
+	// url to update the department id when a new Teacher added
 	@PutMapping("/teacher/teacherId/{teacherId}/departmentId/{departmentId}")
 	public Teacher updateTeacher(@PathVariable("teacherId")int teacherId, @PathVariable("departmentId") int departmentId){
 		Department department = new Department();
@@ -42,41 +44,50 @@ public class ITeacherController {
 		return teacherService.updateTeacher(teacher);
 	}
 	
+	// url to get the teacher by Id
 	@GetMapping("teacher/teacherId/{teacherId}")
 	public Teacher getTeacherById(@PathVariable ("teacherId") int teacherId) {
 		return teacherService.getById(teacherId);
 	}
 	
+	// url to get the list of all the teachers
 	@GetMapping("/teacher")
 	public List<Teacher> getAll(){
 		return teacherService.getAll();
 	}
 	
+	// url to show the full teacher schedule that is assigned
 	@GetMapping("teacher/schedule/{teacherId}")
 	List<Lecture> showTeacherSchedule(@PathVariable("teacherId") int teacherId){
 		return lectureService.showTeacherSchedule(teacherId);
 	}
+	
+	// url to show the today teacher schedule that is assigned
 	@GetMapping("teacher/todayschedule/date/{date}/teacherId/{teacherId}")
 		Lecture showTodayLecture(@PathVariable("date") String date, @PathVariable("teacherId") int teacherId) {
 		LocalDate date1= LocalDate.parse(date);	
 		return lectureService.showTodayLecture(date1, teacherId);
 		}
-		
+	
+	// url to show the teacherName who is having first Lecture in thier class
 	@GetMapping("teacher/firstLec/date/{date}/className/{className}")
 		String showTeacherForFirstLec(@PathVariable("date") String date, @PathVariable("className") String className) {
 		LocalDate date1= LocalDate.parse(date);		
 		return lectureService.getTeacherForFirstLec(date1, className);
 		}
+	// url to show the teacherName who is having second Lecture in thier class
 	@GetMapping("teacher/secondLec/date/{date}/className/{className}")
 		String showTeacherForSecondLec(@PathVariable("date") String date, @PathVariable("className") String className) {
 		LocalDate date1= LocalDate.parse(date);		
 		return lectureService.getTeacherForSecondLec(date1, className);
 		}
+	// url to show the teacherName who is having third Lecture in thier class
 	@GetMapping("teacher/thirdLec/date/{date}/className/{className}")
 		String showTeacherForThirdLec(@PathVariable("date") String date, @PathVariable("className") String className) {
 		LocalDate date1= LocalDate.parse(date);		
 		return lectureService.getTeacherForThirdLec(date1, className);
 		}
+	// url for the teacher to apply for leaves
 	@GetMapping("teacher/leave/date/{date}/teacherId/{teacherId}")
 		String applyLeave(@PathVariable("date") String date, @PathVariable("teacherId") int teacherId) {
 		LocalDate date1= LocalDate.parse(date);		
@@ -84,11 +95,12 @@ public class ITeacherController {
 		return "leave Granted";
 		}
 		
+	// url to add a new Lecture
 	@PostMapping("/teacher/lecture")
 	public Lecture addLecture(@RequestBody Lecture lecture){
 		return lectureService.addLecture(lecture);
 	}
-	
+	// url to assign the lecture to the teacher
 	@PutMapping("/teacher/lecture/lectureId/{lectureId}/teacherId/{teacherId}")
 	public Lecture assignLectureToTeacher(@PathVariable("lectureId")int lectureId, @PathVariable("teacherId") int teacherId){
 		Teacher teacher = new Teacher();
@@ -97,7 +109,7 @@ public class ITeacherController {
 		lecture.setTeacher(teacher);
 		return lectureService.updateLecture(lecture);
 	}
-	
+	// url to get all the lectures details
 	@GetMapping("/teacher/lectures")
 	public List<Lecture> getAllLectures(){
 		return lectureService.getAllLectures();
