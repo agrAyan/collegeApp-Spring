8bbc0eaf4244c763ede34df0b38187b4e4e92d4e
agrAyan <87567431+agrAyan@users.noreply.github.com>
2021-10-18 11:10:07 +0530

    Update CollegeController.java

--- src/main/java/com/collegeapp/controller/CollegeController.java
+++ src/main/java/com/collegeapp/controller/CollegeController.java
@@ -22,30 +22,40 @@ public class CollegeController {
 	@Autowired
 	ICollegeService collegeService;
 	
+	//url to get the college by city
 	@GetMapping("/college/city/{city}")
 	List<College> findByCity(@PathVariable("city") String city){
 		List<College> collegeCity= collegeService.getByCity(city);
 		return collegeCity;
 	}
+	//url to get the college by less fees
 	@GetMapping("/college/fees/{fees}")
 	List<College> findByFeesLessThan(@PathVariable("fees") double fees){
 		List<College> collegeFees= collegeService.getByLessFees(fees);
 		return collegeFees;
 	}
+	
+	//url to delete the college
 	@DeleteMapping("/college/id/{id}")
 	void deleteCollege(@PathVariable("id") int collegeId) {
 		
 		collegeService.deleteCollege(collegeId);
 	}
+	
+	/url to get the college by id
 	@GetMapping("/college/id/{id}")
 	 College getById(@PathVariable("id") int id) {
 		 College college= collegeService.getById(id);
 		 return college;
 	 }
+	
+	// url to update the college details
 	@PutMapping("/college")
 	College updateCollege(@RequestBody College college) {
 		return collegeService.updateCollege(college);
 	}
+	
+	//url to get all the colleges list
 	@GetMapping("/college")
 	List<College> getAllCollege(){
 		return collegeService.getAllCollege();
