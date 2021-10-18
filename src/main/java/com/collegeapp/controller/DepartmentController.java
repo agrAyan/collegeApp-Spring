9a3c6ac985c169b9532ea883c6231bcdc7a4596d
agrAyan <87567431+agrAyan@users.noreply.github.com>
2021-10-18 11:14:52 +0530

    Update DepartmentController.java

--- src/main/java/com/collegeapp/controller/DepartmentController.java
+++ src/main/java/com/collegeapp/controller/DepartmentController.java
@@ -29,45 +29,61 @@ public class DepartmentController {
 	@Autowired
 	ITeacherService teacherService;
 	
+	
+	// url to add a department
 	@PostMapping("/department")
 	public Department addDepartment(@RequestBody Department department) {
 		return departmentService.addDepartment(department);
 	}
 	
+	// url to get the list of all the departments
 	@GetMapping("/department")
 	List<DepartmentDto> showDepartments(){
 		List<DepartmentDto> allDepartment=departmentService.showDepartments();
 		return allDepartment;
 	}
+	
+	// url to get the list of Teacher who is working inside one department
 	@GetMapping("/department/showDeptTeacher/{departmentName}")
 	List<Teacher> showTeacherByDepartment(@PathVariable("departmentName") String departmentName){
 		List<Teacher> teacherByDepartment=teacherService.showTeacherByDepartment(departmentName);
 		return teacherByDepartment;
 	}
+	
+	// url to get the list of the teachers
 	@GetMapping("/department/showTeachers")
 	List<Teacher> getAll(){
 		return teacherService.getAll();
 	}
+	
+	// url to get the teacher list by subject he is and teaching and working inside one department
 	@GetMapping("/department/subject/{subject}/deptName/{departmentName}")
 	List<Teacher> showBySubjectAndDept(@PathVariable("subject") String subject, @PathVariable("departmentName") String departmentName){
 		List<Teacher> teacherBySubAndDept=teacherService.showBySubjectAndDept(subject, departmentName);
 		return teacherBySubAndDept;
 	}
+	
+	// url to get the teacher details by teacherId
 	@GetMapping("/department/teacherId/{id}")
 	Teacher getById(@PathVariable("id") int teacherId) {
 		Teacher teacher= teacherService.getById(teacherId);
 		return teacher;
 	}
+	
+	// url to get the teacher details by techerName
 	@GetMapping("/department/teacherName/{teacherName}")
 	List<Teacher> getByTeacherName(@PathVariable("teacherName") String teacherName){
 		List<Teacher> teacherList=teacherService.getByTeacherName(teacherName);
 		return teacherList;
 	}
+	
+	// url to get the departement details by departmentId
 	@GetMapping("/department/id/{id}")
 	 public DepartmentDto getByDepartmentId(@PathVariable("id") int departmentId) {
 		 return departmentService.getById(departmentId);
 	 }
 	
+	// url to update the college Id after adding a new department 
 	@PutMapping("/department/departmentId/{departmentId}/collegeId/{collegeId}")
 	public Department assignDepartmentToCollege(@PathVariable("departmentId") int departmentId, @PathVariable("collegeId") int collegeId) {
 		College college = new College();
